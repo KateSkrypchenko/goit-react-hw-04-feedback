d3375a3e43cf44760fe292841f5e6dc3fc39ce38
user <katalonij440@gmail.com>
2023-01-26 16:07:54 +0200

    start

---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const TextStyle = styled.p`
+  margin-bottom: 10px;
+
+  &: last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const SpanStyle = styled.span`
+  font-weight: bold;
+`;
